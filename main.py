c7130d86e5a33b7e171f0b6a7940384be49ddbb8
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-08-07 08:17:24 +0300

    переменные окружения грузятся один раз, избавился от обертки над requests, переписал все заново

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
+import os
 from os.path import dirname, join
 from pathlib import Path
 from random import randrange
-
+from pprint import pprint
 import requests
 from dotenv import load_dotenv
 
-from image_helpers import *
-from vk_methods import *
+from image_helpers import get_image_extension, save_image
 
 
 def download_comix(url):
@@ -27,27 +27,112 @@ def save_comix(comix, comix_id):
     return file_path
 
 
-def main():
+def download_random_comix():
     current_comix = download_comix("https://xkcd.com/info.0.json")
     last_comix = current_comix["num"]
     comix_id = randrange(1, last_comix)
+    url = f"https://xkcd.com/{comix_id}/info.0.json"
+    comix = download_comix(url)
+    return comix
+
+
+def get_upload_fields(access_token, group_id):
+    params = {"group_id": group_id, "access_token": access_token, "v": 5.131}
+    response = requests.get(
+        f"https://api.vk.com/method/photos.getWallUploadServer", params=params
+    )
+    response.raise_for_status()
+    response = response.json()
+    return response["response"]
+
+
+def send_photo(upload_fields, image_path, group_id, access_token):
+    with open(image_path, "rb") as file:
+        url = upload_fields["upload_url"]
+        files = {
+            "file": file,  # Вместо ключа "media" скорее всего нужно подставить другое название ключа. Какое конкретно см. в доке API ВК.
+        }
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+        uploaded_file = response.json()
+    if uploaded_file["photo"] != "":
+        uploaded_file["group_id"] = group_id
+        uploaded_file["access_token"] = access_token
+        uploaded_file["v"] = 5.131
+        response_image = requests.post(
+            "https://api.vk.com/method/photos.saveWallPhoto", params=uploaded_file
+        )
+        response_image.raise_for_status()
+        saved_image = response_image.json()
+        return saved_image["response"]
+
+
+def public_post(saved_image, text, group_id, access_token):
+    media_id = saved_image[0]["id"]
+    image_owner_id = saved_image[0]["owner_id"]
+    owner_id = f"-{group_id}"
+    attachments = f"photo{image_owner_id}_{media_id}"
+    params = {
+        "attachments": attachments, 
+        "message": text, 
+        "owner_id": owner_id,
+        "access_token":access_token,
+        "v":5.131
+        }
+    response = requests.post("https://api.vk.com/method/wall.post", params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def main():
+    dotenv_path = join(dirname(__file__), ".env")
+    load_dotenv(dotenv_path)
     try:
-        url = f"https://xkcd.com/{comix_id}/info.0.json"
-        comix = download_comix(url)
+        access_token = os.environ["ACCESS_TOKEN"]
+    except KeyError as e:
+        print("Вы не указали ACCESS_TOKEN")
+        return
+
+    try:
+        group_id = os.environ["GROUP_ID"]
+    except KeyError as e:
+        print("Вы не указали GROUP_ID")
+        return
+
+    try:
+        app_id = os.environ["APP_ID"]
+    except KeyError as e:
+        print("Вы не указали APP_ID")
+        return
+
+    try:
+        comix = download_random_comix()
     except requests.HTTPError as e:
         print(e)
-
+    comix_id = comix["num"]
+    comix_text = comix["alt"]
     image_url = comix["img"]
     extension = get_image_extension(image_url)
     image_name = f"{comix_id}{extension}"
     image_path = save_image(image_url, image_name)
     comix_path = save_comix(comix, comix_id)
     try:
-        saved_image = save_wall_photo(image_path)
-    except HTTPError as e:
-        exit(e)
+        server_address = get_upload_fields(access_token, group_id)
+    except requests.HTTPError as e:
+        print(e)
+        return
 
-    public_post(saved_image, comix["alt"])
+    try:
+        saved_image = send_photo(server_address, image_path, group_id, access_token)
+    except requests.HTTPError as e:
+        print(e)
+        return
+
+    try:
+        post = public_post(saved_image, comix_text, group_id, access_token)
+    except requests.HTTPError as e:
+        print(e)
+        return
     Path.unlink(image_path)
     Path.unlink(comix_path)
 
