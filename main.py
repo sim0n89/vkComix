b9b28afc76f938c70b9d18f4eee936cde22a1d93
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-08-08 08:38:18 +0300

    форматирование кода

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from os.path import dirname, join
 from pathlib import Path
 from random import randrange
-from pprint import pprint
 import requests
 from dotenv import load_dotenv
 
@@ -14,32 +13,28 @@ def download_random_comix():
     response_num_comix = response_num_comix.json()
     count_comixes = response_num_comix["num"]
     comix_id = randrange(1, count_comixes)
-
     url = f"https://xkcd.com/{comix_id}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
     comix = response.json()
-
     comix_id = comix["num"]
     comix_text = comix["alt"]
     image_url = comix["img"]
     extension = get_image_extension(image_url)
     image_name = f"{comix_id}{extension}"
     image_path = save_image(image_url, image_name)
-    saved_comix = {"image_path": image_path, "text": comix_text}
-    return saved_comix
+    return image_path, comix_text
 
 
 def upload_photo(vk_token, group_id, image_path):
     params = {"group_id": group_id, "access_token": vk_token, "v": 5.131}
     response = requests.get(
-        f"https://api.vk.com/method/photos.getWallUploadServer", params=params
+        "https://api.vk.com/method/photos.getWallUploadServer", params=params
     )
     response.raise_for_status()
     response = response.json()
     upload_url = response["response"]["upload_url"]
     with open(image_path, "rb") as file:
-        url = upload_url
         files = {
             "file": file,
         }
@@ -55,8 +50,8 @@ def save_wall_photo(uploaded_file, group_id, vk_token):
         uploaded_file["access_token"] = vk_token
         uploaded_file["v"] = 5.131
         response_image = requests.post(
-            "https://api.vk.com/method/photos.saveWallPhoto", params=uploaded_file
-        )
+            "https://api.vk.com/method/photos.saveWallPhoto", 
+            params=uploaded_file)
         response_image.raise_for_status()
         saved_image = response_image.json()
         return saved_image["response"]
@@ -73,7 +68,10 @@ def publish_post(media_id, owner_id, text, group_id, vk_token):
         "from_group": 1,
         "v": 5.131,
     }
-    response = requests.post("https://api.vk.com/method/wall.post", params=params)
+    response = requests.post(
+        "https://api.vk.com/method/wall.post", 
+        params=params
+    )
     response.raise_for_status()
     return response.json()
 
@@ -83,24 +81,23 @@ def main():
     load_dotenv(dotenv_path)
     try:
         vk_token = os.environ["VK_TOKEN"]
-    except KeyError as e:
+    except KeyError:
         print("Вы не указали VK_TOKEN")
         return
 
     try:
         vk_group_id = os.environ["VK_GROUP_ID"]
-    except KeyError as e:
+    except KeyError:
         print("Вы не указали VK_GROUP_ID")
         return
-    
+
     path = Path("images")
     path.mkdir(exist_ok=True)
-    
+
     try:
-        comix = download_random_comix()
+        image_path, comix_text = download_random_comix()
     except requests.HTTPError as e:
         print(e)
-    image_path = comix["image_path"]
 
     try:
         uploaded_file = upload_photo(vk_token, vk_group_id, image_path)
@@ -119,13 +116,12 @@ def main():
 
     owner_id = saved_image[0]["owner_id"]
     media_id = saved_image[0]["id"]
+
     try:
-        post = publish_post(media_id, owner_id, comix["text"], vk_group_id, vk_token)
+        publish_post(media_id, owner_id, comix_text, vk_group_id, vk_token)
     except requests.HTTPError as e:
         print(e)
         return
-
-    pprint(post)
 
 
 if __name__ == "__main__":
